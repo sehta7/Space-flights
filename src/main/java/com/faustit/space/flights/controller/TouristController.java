be618fa1ca6e678d468c6c4cf2afae6c24f4fbc1
sehta7@gmail.com <sehta7@gmail.com>
2018-09-29 11:40:49 +0200

    change method to home page from String to ModelAndView return type; add method to show list of tourists

--- src/main/java/com/faustit/space/flights/controller/TouristController.java
+++ src/main/java/com/faustit/space/flights/controller/TouristController.java
@@ -1,9 +1,16 @@
 package com.faustit.space.flights.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.faustit.space.flights.model.Flight;
+import com.faustit.space.flights.model.Tourist;
 import com.faustit.space.flights.service.TouristService;
 
 @Controller
@@ -14,7 +21,27 @@ public class TouristController {
 	TouristService touristService;
 	
 	@RequestMapping("/")
-	public String home() {
-		return "tourist/home";
+	public ModelAndView home() {
+		ModelAndView model = new ModelAndView("tourist/home");
+		return model;
 	}
+	
+	@RequestMapping("/list")
+	public ModelAndView list() {
+		List<Tourist> touristsList = touristService.touristsList();
+		System.out.println("Controller --> " + touristsList.toString());
+		List<Flight> flightsList = null;
+		ModelAndView modelAndView = new ModelAndView("tourist/list", "touristsList", touristsList);
+		for (Tourist tourist : touristsList) {
+			flightsList = touristService.flightsList(tourist);
+			modelAndView.addObject("flightsList", flightsList);
+		}
+		return modelAndView;
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add() {
+		return "tourist/add";
+	}
+	
 }
