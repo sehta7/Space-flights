82289ac422ab44888fafe9452a60ec6c029ac992
sehta7@gmail.com <sehta7@gmail.com>
2018-10-01 15:30:07 +0200

    add method to show list od flights to controller class

--- src/main/java/com/faustit/space/flights/controller/FlightController.java
+++ src/main/java/com/faustit/space/flights/controller/FlightController.java
@@ -1,15 +1,36 @@
 package com.faustit.space.flights.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.faustit.space.flights.model.Flight;
+import com.faustit.space.flights.model.Tourist;
+import com.faustit.space.flights.service.FlightService;
 
 @Controller
 @RequestMapping("/flight")
 public class FlightController {
 
+	@Autowired
+	FlightService flightService;
+	
 	@RequestMapping("/")
 	public ModelAndView home() {
 		return new ModelAndView("flight/home");
+	}
+	
+	@RequestMapping("/flights")
+	public ModelAndView flights() {
+		List<Flight> flightsList = flightService.flightsList();
+		for (Flight flight : flightsList) {
+			List<Tourist> touristsList = flightService.touristsList(flight);
+			flight.setTourists(touristsList);
+		}
+		ModelAndView modelAndView = new ModelAndView("flight/flights", "flightsList", flightsList);
+		return modelAndView;
 	}
 }
